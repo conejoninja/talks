b8923a6890fb3fd8cd8f64951e6321e20a04de12
Daniel Esteban <conejo@conejo.me>
2020-02-02 12:34:09 +0100

    added fosdem talk slides

---
+++ 2020/fosdem/snippets/go1.go
@@ -0,0 +1,71 @@
+package mygolibrary
+
+// HL
+func Hello(name string) string {
+	return "Hello " + name + "! (fromk go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}package mygolibrary
+
+func Hello(name string) string {
+	return "Hello " + name + "! (from go)"
+}
+}
